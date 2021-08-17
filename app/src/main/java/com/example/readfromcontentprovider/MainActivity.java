b3069aa270128c0186b626e5312c056df34dddaa
Zeeshan Rasool <m.zeeshan.rasool@gmail.com>
2021-08-17 17:26:29 +0400

    Second Commit

--- app/src/main/java/com/example/readfromcontentprovider/MainActivity.java
+++ app/src/main/java/com/example/readfromcontentprovider/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
@@ -13,15 +16,44 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.readfromcontentprovider.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        onClickShowDetails(binding.btnRetrieve);
+        Objects.requireNonNull(binding.txtName.getEditText()).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.txtName.setError(null);
+            }
+        });
     }
 
     public void onClickAddDetails(View view) {
+        String name = Objects.requireNonNull(binding.txtName.getEditText()).getText().toString();
+        if(TextUtils.isEmpty(name)){
+            binding.txtName.setError("Cannot be Empty!");
+            return;
+        }
         final String PROVIDER_NAME = "com.example.readfromcontentprovider.Birthday";
         final String URL = "content://" + PROVIDER_NAME + "/users";
         final Uri CONTENT_URI = Uri.parse(URL);
@@ -29,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         values.put("name", "ZEESHAN RASOOL");
         getContentResolver().insert(CONTENT_URI, values);
         Toast.makeText(getBaseContext(), "New Record Inserted", Toast.LENGTH_LONG).show();
+        onClickShowDetails(binding.btnRetrieve);
     }
 
 
